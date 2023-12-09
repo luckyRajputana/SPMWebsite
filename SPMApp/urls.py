65afb8ecf0774bc13e6250183bb6e070caaa84dd
Lucky <lucky.chauhan14994.com>
2023-12-09 13:55:07 +0530

    fav commit

--- SPMApp/urls.py
+++ SPMApp/urls.py
@@ -10,5 +10,5 @@ urlpatterns = [
     path('get_column_data/', views.GetColumnDataView, name='get_column_data'),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
