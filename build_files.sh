bd59c484290fd7763e5c53cb87b9b60a299bbf91
Lucky <lucky.chauhan14994.com>
2023-12-09 15:22:59 +0530

    latest one commit 3

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,5 @@
 echo "BUILD START"
-echo "$(lsb_release -a)"
-sudo yum install postgresql-devel
+apt-get install libpq-dev
 pip install -r requirements.txt
 python3.9 manage.py collectstatic
 echo "BUILD END"
