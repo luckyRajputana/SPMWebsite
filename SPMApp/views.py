78a3ffd5b4fe56b7bee5583d9fc4eecb806a1900
Lucky <46166986+luckyRajputana@users.noreply.github.com>
2024-03-10 14:19:34 +0530

    Update views.py
    commit

--- SPMApp/views.py
+++ SPMApp/views.py
@@ -1,9 +1,13 @@
 import traceback
 
+from django.db import transaction
 from django.http import JsonResponse
 from django.shortcuts import render, get_object_or_404,redirect
 from .forms import *
 from .models import mainpagetable
+from django.core.files.storage import FileSystemStorage
+import pandas as pd
+from datetime import datetime
 
 def MainPage(request):
     if request.method == 'POST':
@@ -143,3 +147,73 @@ def GetColumnDataView(request):
     items_v = list(sorted(set(items)))
     items_v.append("other")
     return JsonResponse({'vendors':vendors ,'companies': companies,'items_v': items_v})
+
+
+def upload_excel(request):
+    if request.method == 'POST' and request.FILES['excel_file']:
+        try:
+            print("Inside")
+            excel_file = request.FILES['excel_file']
+            fs = FileSystemStorage()
+            filename = fs.save(excel_file.name, excel_file)
+            uploaded_file_url = fs.url(filename)
+            current_timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            # Process the uploaded Excel file
+            df = pd.read_excel(excel_file)  # Read the Excel file using pandas
+            excel_columns = list(df.columns)
+            model_meta = mainpagetable._meta
+
+            # Extract column names from the model's meta information
+            expected_columns = [field.name for field in model_meta.get_fields()]
+            print(expected_columns[1:-1])
+            expected_columns = expected_columns[1:-1]
+
+            # Compare the extracted column names with the expected column names
+            missing_columns = [col for col in expected_columns if col not in excel_columns]
+            extra_columns = [col for col in excel_columns if col not in expected_columns]
+            success = ""
+            # Perform validation based on the comparison results
+            if missing_columns:
+                failure = f"Missing columns: {', '.join(missing_columns)}"
+            elif extra_columns:
+                failure =  f"Extra columns: {', '.join(extra_columns)}"
+            else:
+                success =  "Column names validation successful"
+            # Do further processing as needed, such as data manipulation, analysis, etc.
+            if success == "Column names validation successful":
+                failed_ingestions = []
+                failed_string = ""
+                for index, row in df.iterrows():
+                    try:
+                        with transaction.atomic():
+                            mainpagetable.objects.create(VendorType=row["VendorType"], CompanyType=row["CompanyType"],
+                                                 ItemType=row["ItemType"], ModelNo=row["ModelNo"],HSNCode = row["HSNCode"], \
+                                                PriceFromChina = row["PriceFromChina"], DeclaredCustomPrice = row["DeclaredCustomPrice"],
+                                                SellingPrice = row["SellingPrice"],
+                                                 DateTime=current_timestamp)
+                    except Exception as e:
+                        # If ingestion fails for a row, add it to the list of failed ingestions
+                        index_detail = str(e).find("DETAIL:")
+
+                        # Extract the substring after "DETAIL:"
+                        if index_detail != -1:
+                            detail_string = str(e)[index_detail + len("DETAIL:"):]
+
+                        failing =str(row["VendorType"] )+ ' and ' + str(row["ModelNo"] ), "Error : "+detail_string
+                        print(failing)
+                        print(type(str(failing)))
+                        failed_ingestions.append(str(failing))
+
+                failed_string = ', '.join(failed_ingestions)
+                success_message_file_upload = "Excel file uploaded!"
+            return render(request, 'MainPageTemplate.html', {
+                'success_message_file_upload': success_message_file_upload,
+                'error_message_file_upload' : "Failed rows :  "+ failed_string
+            })
+
+        except Exception as e:
+            return render(request, 'MainPageTemplate.html', {
+                'error_message_file_upload' : failure
+            })
+
+    return render(request, 'MainPageTemplate.html')
