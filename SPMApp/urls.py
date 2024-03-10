0f06affdc49a780095a66c254e93b55ce3924647
Lucky <46166986+luckyRajputana@users.noreply.github.com>
2024-03-10 14:19:52 +0530

    Update urls.py
    commit

--- SPMApp/urls.py
+++ SPMApp/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('getmodeldetails/', views.FetchModelDetails, name='FetchModelDetails'),
     path('getmodeldetails/<int:pk>/delete/', views.RowDeleteView, name='Row_delete'),
     path('get_column_data/', views.GetColumnDataView, name='get_column_data'),
+    path('upload/', views.upload_excel, name='upload_excel'),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
